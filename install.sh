b032646fb1457557af692d6f47b97f1e17a7de7b
Eddy de Boer <eddy147@gmail.com>
2021-12-11 17:10:58 +0100

    first commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo apt install --yes libssl-dev automake autoconf libncurses5-dev
+
+asdf install erlang 24.1.7
+asdf install elixir master-otp-24
+
+sudo apt-get --yes install build-essential autoconf m4 libncurses5-dev libwxgtk3.0-gtk3-dev libwxgtk-webview3.0-gtk3-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk
+
+git clone git@github.com:eddy147/config-nvim.git ~/.config/nvim
+
+alias vim="nvim"
