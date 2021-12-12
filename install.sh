39f8a4431f8fab255256938af87ef919eb96296e
eddy147 <eddy147@gmail.com>
2021-12-12 13:54:50 +0100

    cleaning up

--- install.sh
+++ install.sh
@@ -2,19 +2,19 @@ cd $HOME
 sudo apt update
 sudo apt upgrade
 sudo apt --yes install curl git zsh
+sudo apt-get install aptitude
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sh install.sh
 
-sudo apt install --yes libssl-dev automake autoconf libncurses5-dev
+sudo apt-get --yes install libssl-dev automake build-essential autoconf m4 libncurses5-dev libwxgtk3.0-gtk3-dev libwxgtk-webview3.0-gtk3-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk
 
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 echo ". $HOME/.asdf/asdf.sh" >> ~/.zshrc
 source ~/.zshrc
+
 asdf install erlang 24.1.7
 asdf install elixir master-otp-24
-
-sudo apt-get --yes install build-essential autoconf m4 libncurses5-dev libwxgtk3.0-gtk3-dev libwxgtk-webview3.0-gtk3-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk
 
 git clone git@github.com:eddy147/config-nvim.git ~/.config/nvim
 
@@ -26,6 +26,4 @@ git config --global user.email "eddy147@gmail.com"
 git config --global user.name "eddy147"
 
 alias vim="nvim"
-sudo apt-get install aptitude
 sudo apt install silversearcher-ag
-echo ". /home/eddy/.asdf/asdf.sh" >> ~/.zshrc
