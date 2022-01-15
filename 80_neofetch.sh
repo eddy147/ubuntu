deff4a118fad73696bef1984e486dc32fd2b4aaf
eddy147 <eddy147@gmail.com>
2022-01-15 14:26:20 +0100

    themes for terminal

--- 80_neofetch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo add-apt-repository --yes ppa:dawidd0811/neofetch
-sudo apt update --yes && sudo apt install --yes neofetch
-neofetch
---
+++ 80_nice_to_haves.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo add-apt-repository --yes ppa:dawidd0811/neofetch
+sudo apt update --yes && sudo apt install --yes neofetch
+
+sudo curl -Lo /usr/bin/theme.sh 'https://git.io/JM70M' && sudo chmod +x /usr/bin/theme.sh
+theme.sh farin 
+echo "See https://github.com/lemnos/theme.sh for examples"
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
