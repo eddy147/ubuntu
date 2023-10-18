66339e1c857e495906b642bfd848330279782801
eddy147 <eddy147@gmail.com>
2023-10-18 09:51:40 +0200

    docker

--- 40_docker.sh
+++ 40_docker.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-sudo apt install --yes \
-docker \
-docker-compose 
+sudo apt update -y && sudo apt upgrade -y
+sudo apt remove docker docker-engine docker.io containerd runc
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+sudo apt update -y
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl start docker
+sudo systemctl enable docker
+
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo apt install docker-compose
