d0d8a152a9dcf305b13c24a87de88babb394c746
Eddy de Boer <eddy147@gmail.com>
2024-04-02 13:51:14 +0200

    copy from vim to clipboard

--- 00_main.sh
+++ 00_main.sh
@@ -2,53 +2,54 @@
 
 sudo apt update --yes
 sudo apt upgrade --yes
-sudo apt --yes install curl 
-sudo apt --yes install aptitude 
-sudo apt --yes install libssl-dev 
-sudo apt --yes install automake 
-sudo apt --yes install build-essential 
-sudo apt --yes install autoconf 
-sudo apt --yes install m4 
-sudo apt --yes install libncurses5-dev 
-sudo apt --yes install libwxgtk3.0-gtk3-dev 
-sudo apt --yes install libwxgtk-webview3.0-gtk3-dev 
-sudo apt --yes install libgl1-mesa-dev 
-sudo apt --yes install libglu1-mesa-dev 
-sudo apt --yes install libpng-dev 
-sudo apt --yes install libssh-dev 
+sudo apt --yes install curl
+sudo apt --yes install aptitude
+sudo apt --yes install libssl-dev
+sudo apt --yes install automake
+sudo apt --yes install build-essential
+sudo apt --yes install autoconf
+sudo apt --yes install m4
+sudo apt --yes install libncurses5-dev
+sudo apt --yes install libwxgtk3.0-gtk3-dev
+sudo apt --yes install libwxgtk-webview3.0-gtk3-dev
+sudo apt --yes install libgl1-mesa-dev
+sudo apt --yes install libglu1-mesa-dev
+sudo apt --yes install libpng-dev
+sudo apt --yes install libssh-dev
 sudo apt --yes install libglib2.0-dev
-sudo apt --yes install unixodbc-dev 
-sudo apt --yes install xsltproc 
-sudo apt --yes install fop 
-sudo apt --yes install libxml2-utils 
-sudo apt --yes install libncurses-dev 
-sudo apt --yes install openjdk-11-jdk 
+sudo apt --yes install unixodbc-dev
+sudo apt --yes install xsltproc
+sudo apt --yes install fop
+sudo apt --yes install libxml2-utils
+sudo apt --yes install libncurses-dev
+sudo apt --yes install openjdk-11-jdk
 sudo apt --yes install silversearcher-ag
 sudo apt --yes install inotify-tools
 sudo apt --yes install graphviz
 sudo apt --yes install jq
 sudo apt --yes install python3-pip
-sudo apt --yes install ninja-build 
-sudo apt --yes install gparted 
-sudo apt --yes install jq
+sudo apt --yes install ninja-build
+sudo apt --yes install gparted
+sudo apt --yes install xclip
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/eddy/.zshrc
+(
+	echo
+	echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"'
+) >>/home/eddy/.zshrc
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install gcc
 
 # otherwise scylla can not run: (see https://docs.ondat.io/docs/prerequisites/max-aio/)
-sudo "fs.aio-max-nr=1048576" >> /etc/sysctl.conf
+sudo "fs.aio-max-nr=1048576" >>/etc/sysctl.conf
 
-./10_git.sh && 
-./20_zsh.sh && 
-./21_fzf.sh &&
-./30_asdf.sh && 
-./40_docker.sh && 
-./50_elixir.sh &&  
-#./60_neovim.sh && 
-#./70_pgadmin.sh && 
-./80_neofetch && 
-
-echo "Reboot for all installations to take effect."
-
+./10_git.sh &&
+	./20_zsh.sh &&
+	./21_fzf.sh &&
+	./30_asdf.sh &&
+	./40_docker.sh &&
+	./50_elixir.sh &&
+	#./60_neovim.sh &&
+	#./70_pgadmin.sh &&
+	./80_neofetch &&
+	echo "Reboot for all installations to take effect."
--- 60_neovim.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo apt install --yes neovim
-git clone git@github.com:eddy147/config-nvim.git ~/.config/nvim
-
-echo "alias vim=\"nvim\"" >> ~/.zshrc
