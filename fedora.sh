9681265e320aab1248400dcb56f80b2d3311b94f
eddy <eddy147@gmail.com>
2025-03-25 09:40:04 +0100

    fedora

---
+++ fedora.sh
@@ -0,0 +1,7 @@
+sudo dnf install -y \
+    make gcc zlib-devel bzip2 bzip2-devel readline-devel \
+    sqlite sqlite-devel openssl-devel tk-devel libffi-devel \
+    xz-devel curl expat-devel \
+    gcc-c++ python3-devel uuid-devel \
+    libnsl2-devel ncurses-devel
+
